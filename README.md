2d5736ed8c6fa44c02ebe29326c2029248567e70
Ed0ch <carlos.e.8a444@gmailcom>
2023-10-23 21:28:41 -0600

    modified:   README.md 	new file:   app.py 	new file:   notebooks/EDA.ipynb 	new file:   requirements.txt 	new file:   vehicles_us.csv

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Projetct-S4
+
+La aplicaciónm fuinciona para danros información acerca de los vehículos
---
+++ app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+scatter_button=st.button('Construir gráfico de dispersión')        
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True) 
+
+
+if scatter_button:
+    
+    st.write('Creacion de un diagrama de dispersión')
+
+    fig=px.scatter(car_data)
+
+    st.plotly_chart(fig,use_container_width=True)
+
