801e5af44e09b0ab784cfefde800288c5fc49353
Rodo-ASUS <rununez@entel.cl>
2023-10-24 23:00:02 -0300

    cambio a ruta relativa

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('../../datasets/vehicles_us.csv') # leer los datos
+car_data = pd.read_csv('files/datasets/input/vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
 scatter_button=st.button('Construir gráfico de dispersión')        
 if hist_button: # al hacer clic en el botón
