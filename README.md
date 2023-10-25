3601b8ac17f23fd7297c56d676902821c23c8033
Ed0ch <carlos.e.8a444@gmailcom>
2023-10-24 18:22:12 -0600

    new file:   .streamlit 	modified:   README.md 	modified:   app.py

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Projetct-S4
 
-La aplicaciónm fuinciona para danros información acerca de los vehículos
+La aplicaciónm fuinciona para danros información acerca de los vehículos, en donde podemos ver la distribución de los datos a través del histograma dandole clcik a un botón, al igual que con el diagrama de dispersión.
+Streamlit nos permite correr el documento de una forma más interactiva.
--- app.py
+++ app.py
@@ -2,25 +2,31 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-scatter_button=st.button('Construir gráfico de dispersión')        
-if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
+# Leer los datos desde un archivo CSV
+car_data = pd.read_csv('vehicles_us.csv')
+
+# Crear botones para construir el histograma y el gráfico de dispersión
+hist_button = st.button('Construir histograma')
+scatter_button = st.button('Construir gráfico de dispersión')
+
+# Verificar si se hizo clic en el botón de histograma
+if hist_button:
+    # Escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
+
+    # Crear el histograma
     fig = px.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True) 
 
+    # Mostrar el gráfico Plotly
+    st.plotly_chart(fig, use_container_width=True)
 
+# Verificar si se hizo clic en el botón de gráfico de dispersión
 if scatter_button:
-    
-    st.write('Creacion de un diagrama de dispersión')
+    # Escribir un mensaje
+    st.write('Creación de un diagrama de dispersión')
 
-    fig=px.scatter(car_data)
+    # Crear el gráfico de dispersión
+    fig = px.scatter(car_data)
 
-    st.plotly_chart(fig,use_container_width=True)
-
+    # Mostrar el gráfico Plotly
+    st.plotly_chart(fig, use_container_width=True)
