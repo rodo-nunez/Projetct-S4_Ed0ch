44a4d2262b4baa555fbd8ff4f05a04eef4269c4c
Rodo-ASUS <rununez@entel.cl>
2024-07-17 21:29:12 -0400

    simplificacion

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import streamlit as st
 
 car_data = pd.read_csv('files/datasets/input/vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
-scatter_button=st.button('Construir gráfico de dispersión')        
 if hist_button: # al hacer clic en el botón
             # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -16,11 +15,4 @@ if hist_button: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True) 
 
 
-if scatter_button:
-    
-    st.write('Creacion de un diagrama de dispersión')
-
-    fig=px.scatter(car_data)
-
-    st.plotly_chart(fig,use_container_width=True)
 
